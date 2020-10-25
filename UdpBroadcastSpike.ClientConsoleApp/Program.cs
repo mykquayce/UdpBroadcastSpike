bf5a595e43155e2fb2411ed30a93b308b04159aa
Bob <myk_quayce@hotmail.com>
2020-10-25 20:35:28 +0000

    tweaking for tp-link smart devices

--- UdpBroadcastSpike.ClientConsoleApp/Program.cs
+++ UdpBroadcastSpike.ClientConsoleApp/Program.cs
@@ -10,11 +10,11 @@ namespace UdpBroadcastSpike.ClientConsoleApp
 		static void Main(string[] args)
         {
 			var Client = new UdpClient();                           //Create UdpClient (port number is assigned appropriately)
-			var RequestData = Encoding.ASCII.GetBytes("Request");   //Appropriate request data
+			var RequestData = new byte[] { 208, 242, 129, 248, 139, 255, 154, 247, 213, 239, 148, 182, 209, 180, 192, 159, 236, 149, 230, 143, 225, 135, 232, 202, 240, 139, 246, 139, 246, };   //Appropriate request data
 			var ServerEp = new IPEndPoint(IPAddress.Any, 0);        //Server (communication partner) endpoint ServerEp creation (IP)/Port not specified)
 
 			Client.EnableBroadcast = true;                          //Broadcast enabled
-			Client.Send(RequestData, RequestData.Length, new IPEndPoint(IPAddress.Broadcast, 8888)); //Broadcast to port 8888
+			Client.Send(RequestData, RequestData.Length, new IPEndPoint(IPAddress.Broadcast, 9999)); //Broadcast to port 8888
 
 			//The person who received the transmitted data should have known the endpoint information of himself / herself (client).
 			//Wait for the packet to be sent to it
